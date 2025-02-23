ef675cb1fda3eda32a0a6e83986863ebf09bc244
Yibeltal-0849 <yibeltal2514@gmail.com>
2025-02-23 08:58:32 +0300

    Carousel

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import Header from "../Header/Header";
 import TestTwo from "../Header/test/TestTwo";
 import TestOne from "../Header/test/testOne";
+import Carousels from "../Carousel/Carousels";
 
 const Home = () => {
   return (
     <>
       <Header />
+      <Carousels />
     </>
   );
 };
