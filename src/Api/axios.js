17d62520b75d9bd48722c961cbac0f80bfe641d5
Yibeltal-0849 <yibeltal2514@gmail.com>
2025-03-10 15:29:30 +0300

    backend deploy on render.com

--- src/Api/axios.js
+++ src/Api/axios.js
@@ -1,5 +1,8 @@
 import axios from "axios";
 const axiosInstance = axios.create({
-  baseURL: "http://127.0.0.1:5001/clone-39f6b/us-central1/api",
+  //local instance of firebase function
+  // baseURL: "http://127.0.0.1:5001/clone-39f6b/us-central1/api",
+  //deployed version of amazon server on render.com
+  baseURL: "https://amazon-api-y0px.onrender.com",
 });
 export { axiosInstance };
